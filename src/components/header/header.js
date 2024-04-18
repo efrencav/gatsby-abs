215fce9a41bf657f458fb61604c1524bcacb5a24
Efren Cavazos <efren.cavazos@gmail.com>
2024-04-18 15:23:58 -0500

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import { Link } from 'gatsby';
 import React, { useEffect, useState } from 'react';
 import BurgerMenus from './burgerMenus';
-import HeaderSearch from './headerSearch';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -38,7 +37,7 @@ const Header = () => {
                                     <nav id="mobile-menu">
                                         <ul>
                                             <li className="menu-item-has-children"><Link to="/">Home</Link>
-                                                
+
                                             </li>
                                             <li><Link to="/about">About</Link></li>
                                             <li className="menu-item-has-children"><Link to="/services">Services</Link>
@@ -51,17 +50,13 @@ const Header = () => {
                                             <li className="menu-item-has-children"><Link to="/projects">Portfolio</Link>
                                             </li>
                                             <li className="menu-item-has-children"><Link to="/blogs">Blog</Link>
-                                                
+
                                             </li>
                                             <li><Link to="/contact">Contact</Link>
-                                               
+
                                             </li>
                                         </ul>
                                     </nav>
-                                </div>
-                                <div className="side-menu-icon d-xl-none text-end">
-                                    <span className="search-btn nav-search search-trigger" onClick={() => { setSearchOpen(!searchOpen) }} role='presentation'><i className="fas fa-search"></i></span>
-                                    <button className="side-toggle" onClick={() => { setMenuOpen(!menuOpen) }} role='presentation'><i className="far fa-bars"></i></button>
                                 </div>
                             </div>
                             <div className="col-xl-4 d-none d-xl-block">
@@ -70,7 +65,7 @@ const Header = () => {
                                         <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fab fa-facebook-f"></i></a>
                                         <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
                                     </div>
-                                    
+
                                     <div className="header-btn">
                                         <Link to="/get-a-quote" target="_blank" className="theme-btn theme-btn-small" >Request a Quote</Link>
                                     </div>
@@ -83,9 +78,6 @@ const Header = () => {
 
             <BurgerMenus menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
             <div onClick={() => setMenuOpen(false)} role='presentation' className={menuOpen ? "body-overlay show" : "body-overlay"}></div>
-
-            <HeaderSearch searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
-            <div onClick={() => setSearchOpen(false)} role='presentation' className={searchOpen ? "body-overlay show" : "body-overlay"}></div>
 
         </header>
     );
