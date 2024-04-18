1869cd5d50dc93b10e50f956aa02f2b40671b3f3
Efren Cavazos <efren.cavazos@gmail.com>
2024-04-18 11:22:22 -0500

    Update index.js

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -3,14 +3,16 @@ import Breadcrumb from '../common/breadcrumb';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import SignUpMain from './signUpMain';
+import WhyWeTwo from './whyWeTwo';
 
 const index = () => {
     return (
         <>
             <Header />
             <main>
-                <Breadcrumb breadcrumbTitle="SignUp" breadcrumbSubTitle="SignUp" />
+                <Breadcrumb breadcrumbTitle="Get a Quote" breadcrumbSubTitle="Get a Quote" />
                 <SignUpMain />
+                <WhyWeTwo />
             </main>
             <Footer />
         </>
