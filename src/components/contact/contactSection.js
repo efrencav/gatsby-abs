94d5e48ffa81fecaaae88fe18e16d78c2a173ae8
Efren Cavazos <efren.cavazos@gmail.com>
2024-04-18 13:31:33 -0500

    Update contactSection.js

--- src/components/contact/contactSection.js
+++ src/components/contact/contactSection.js
@@ -41,34 +41,50 @@ const ContactSection = () => {
                         </div>
                     </div>
                     <div className="col-xxl-7 col-xl-6 col-lg-6">
-                        <div className="contact-form">
-                            <form action="#" id="contact-form">
-                                <div className="row">
-                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="name" type="text" placeholder="Your Name" />
+                        <form
+                            name="Contact Us"
+                            method="POST"
+                            data-netlify="true"
+                            onSubmit="submit"
+                            data-netlify-honeypot="bot-field">
+
+                            <p hidden>
+                                <label>
+                                    Don't fill this out: <input name="bot-field" />
+                                </label>
+                            </p>
+                            <input type="hidden" name="form-name" value="Get a Quote" />
+
+                            <div className="contact-form">
+                                <form action="#" id="contact-form">
+                                    <div className="row">
+                                        <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                            <input name="first-name" type="text" placeholder="Your Name" />
+                                        </div>
+                                        <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                            <input name="email" type="email" placeholder="Email Adress" />
+                                        </div>
+                                        <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                            <input name="phone" type="text" placeholder="Phone" />
+                                        </div>
+                                        <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                            <input name="subject" type="text" placeholder="Subject" />
+                                        </div>
+                                        <div className="col-xxl-12 col-xl-12 col-lg-12 mb-20">
+                                            <textarea placeholder="Write Massage" name="massage"></textarea>
+                                        </div>
+                                        <div className="col-xxl-12 col-xl-12 mb-20">
+                                            <button type="submit" className="theme-btn border-btn">Send a message</button>
+                                        </div>
                                     </div>
-                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="email" type="email" placeholder="Email Adress" />
-                                    </div>
-                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="phone" type="text" placeholder="Phone" />
-                                    </div>
-                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="subject" type="text" placeholder="Subject" />
-                                    </div>
-                                    <div className="col-xxl-12 col-xl-12 col-lg-12 mb-20">
-                                        <textarea placeholder="Write Massage" name="massage"></textarea>
-                                    </div>
-                                    <div className="col-xxl-12 col-xl-12 mb-20">
-                                        <button type="submit" className="theme-btn border-btn">Send a message</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                                </form>
+                            </div>
+                        </form>
+
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 };
 
