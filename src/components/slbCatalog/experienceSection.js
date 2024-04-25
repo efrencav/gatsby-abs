39a832b371f3f64121c96cca11f55c36085e9ca4
Efren Cavazos <efren.cavazos@gmail.com>
2024-04-25 18:05:13 -0500

    Add files via upload

--- src/components/slbCatalog/experienceSection.js
+++ src/components/slbCatalog/experienceSection.js
@@ -6,20 +6,23 @@ const ExperienceSection = () => {
     return (
         <section className="project-details-area pt-120 pb-70">
             <div className="container">
-                <div className="row">
+                {/* <div className="row">
                     <div className="col-xxl-12">
                         <div className="project-big-thumb">
-                            <img src="/assets/img/project/project-big-img-slb-catalog.jpg" alt="Open brochure showing inner pages" />
                         </div>
                     </div>
-                </div>
+                </div> */}
                 <div className="row">
                     <div className="col-xxl-8 col-xl-8 col-lg-8 col-md-12">
                         <div className="p-details-content mb-40">
                             <h3>Well Intervention Catalog</h3>
+                            <img className='mb-20' src="/assets/img/project/well-intervention-1.jpg" alt="Open brochure showing inner pages" />
                             <p>Schlumberger had a new segment that needed to inform its audience of its tools, products, and services. The goal was to create new content and visual references for data information of its products, original content for case studies, and tech reports for existing services.</p>
+                            <img className='mb-20' src="/assets/img/project/well-intervention-2.jpg" alt="Open brochure showing inner pages" />
                             <p>Art directed the design of a 3-book edition catalog and a book insert to hold the books that carried over 750 pages of new content, detailing all data of its products, case study, and tech reports information. A mix of photoshoots, custom tool illustrations, and renderings was required to highlight each tool element, resulting in a well-documented catalog design.</p>
+                            <img className='mb-20' src="/assets/img/project/well-intervention-3.jpg" alt="Open brochure showing inner pages" />
                             <p>The Well Intervention Catalog was well received. We shipped 1,500 copies to different customers across the globe. The catalog design received five awards for the following: BMA Lantern Award, Best of Category - Digital - GEA, Best of Category - Offset - GEA, Division 1 - Best of Division - GEA, and Digital Division - Gold Award - GEA.</p>
+                            <img className='mb-20' src="/assets/img/project/well-intervention-4.jpg" alt="Open brochure showing inner pages" />
                         </div>
                     </div>
                     <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
