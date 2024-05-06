1ba8aa3fdfe98b14047170c9d92653b4f0a029c0
Efren Cavazos <efren.cavazos@gmail.com>
2024-05-06 08:17:01 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Abstract Creative – Digital Agency located in Houston, Texas`,
-    description: `We specialize in providing graphic design, website design, branding, and other related services.`,
+    description: `Are you looking for top-notch graphic design, website design, branding, and other related services? Let us help you create a standout brand image that impresses your audience and takes your business to the next level.`,
     author: `@r-ichard`,
   },
   plugins: [
